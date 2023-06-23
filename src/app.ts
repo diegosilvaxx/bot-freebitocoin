284bd2b46628866ec88365b3957cb91a4604a38a
Diego da Silva Borges <33516555+diegosilvaxx@users.noreply.github.com>
2023-06-23 13:43:06 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,10 @@ import botDentao from "./bot-iptv-dentao";
 import message from "./utils/message";
 
 const result = async () => {
-  await venom
-    .create({
-      session: "session-name", //name of session
-      multidevice: false, // for version not multidevice use false.(default: true)
-    })
-    .then((client: any) =>
-      //BOT FREE BITCOIN MULTIPLAY
-      // freebitcoinMultiplay(client)
-
-      //BOT DENTÃO IPTV
-      // botDentao(client, message.msgIptvDentao)
 
       //BOT FREE BITCOIN A CADA 1 HORA
-      freebitcoinrool(client)
-    )
-    .catch((erro: any) => {
-      console.log(erro);
-    });
+      freebitcoinrool()
+
 };
 
 result();
